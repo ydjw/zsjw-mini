0d0ceb1fe217cf0b569ac71754d1741f06101179
xiedong <xiedong11@aliyun.com>
2019-07-24 18:39:07 +0800

    成绩列表接口数据对接

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,29 +1,59 @@
 import Taro, {Component} from "@tarojs/taro";
-import {View} from "@tarojs/components";
+import {Text, View} from "@tarojs/components";
 import Api from '../../api/educations'
+import './style.less'
 
 class HomePage extends Component {
 
-  componentDidMount() {
-    let token = Taro.getStorageSync('token')
-    if (token) {
-      console.log(token);
-    }
+  state = {
+    result: {}
+  }
 
-    console.log(token);
+  componentDidMount() {
+
     let data = {
       'method': 'getCjcx',
       'xh': '2017411881',
     }
     let result = Api.commonGet(data)
-    console.log(result);
+    result.then(res => {
+      this.setState({
+        result: res.data.result
+      })
+    })
+
 
   }
 
   render() {
+
+    let {result} = this.state
     return (
-      <View>
-        dfsadfasdfs
+      <View id='root-view'>
+        {
+          console.log(result)
+        }
+        {
+          result.map((item, index) => {
+            return <View id='item-root' key={index}>
+              <View id='kc-type'>
+                {item.kclbmc}
+              </View>
+              <View id='kc-info'>
+                <Text id='kc-name'>
+                  {item.kcmc}
+                </Text>
+                <Text id='kc-name-type'>
+                  {item.kcxzmc}
+                </Text>
+                <Text id='kc-score'>
+                  学分值:{item.xf}
+                </Text>
+              </View>
+              <Text id='score'>{item.zcj}</Text>
+            </View>
+          })
+        }
       </View>
     )
   }
